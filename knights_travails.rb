21a584b393c92f3ae027e908a6396b0433d4beeb
meowzie <appenge2@gmail.com>
2022-02-07 23:33:58 +0200

    Created a class for the chess board and a class for the positions on the board

---
+++ knights_travails.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Creates chess board
+class Board
+  def initialize; end
+end
+
+# Creates positions on the chess board
+class Position
+  def initialize(horizontal, vertical)
+    @horizontal = horizontal
+    @vertical = vertical
+  end
+end
