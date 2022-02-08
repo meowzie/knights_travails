105652e63d0b5049508e9f7132edd39fdee31a83
meowzie <appenge2@gmail.com>
2022-02-08 20:25:09 +0200

    Created class Knight; Wrote added ability to find all valid moves the knight can make when on a given square; Renamed class Position to Square and combined its horizontal and vertical variables to a single array of coordinates; Renamed @positions to @squares in class Board to better reflect the nature of the variable; Added Board#find to simplify the process of finding a square on the board; Added Board#add_knight to create a night and assign it a position on the board.

--- knights_travails.rb
+++ knights_travails.rb
@@ -2,18 +2,44 @@
 
 # Creates chess board
 class Board
-  attr_accessor :positions
+  attr_accessor :squares, :knight
 
   def initialize
-    @positions = []
-    8.times { |row| 8.times { |column| @positions << [row, column] } }
+    @squares = []
+    8.times { |row| 8.times { |column| @squares << Square.new(row, column) } }
+  end
+
+  def find(row, column)
+    @squares.find { |square| square.coordinates == [row, column] }
+  end
+
+  def add_knight(row, column)
+    @knight = Knight.new(find(row, column))
   end
 end
 
-# Creates positions on the chess board
-class Position
+# Creates squares on the chess board
+class Square
+  attr_accessor :coordinates
+
   def initialize(horizontal, vertical)
-    @horizontal = horizontal
-    @vertical = vertical
+    @coordinates = [horizontal, vertical]
+  end
+end
+
+# Creates the knight (which goes on the chess board)
+class Knight
+  attr_accessor :position
+
+  def initialize(position)
+    @position = position
+    @row = position.coordinates[0]
+    @column = position.coordinates[1]
+  end
+
+  def possible_moves
+    valid = [[@row + 1, @column + 2], [@row + 1, @column - 2], [@row + 2, @column + 1], [@row + 2, @column - 1],
+             [@row - 1, @column + 2], [@row - 1, @column - 2], [@row - 2, @column + 1], [@row - 2, @column - 1]]
+    valid.map { |position| position if position.none? { |coordinate| coordinate.negative? || coordinate > 7 } }.compact
   end
 end
