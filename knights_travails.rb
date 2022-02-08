dde2c27a166f3197611fe82a7a657233603f5b72
meowzie <appenge2@gmail.com>
2022-02-08 23:12:47 +0200

    Created module Element, which contains a method that returns 0 when called on an array and returns 1 otherwise -- this is to allow for placing the valid squares that the knight may move to before any nil values in Knight#possible_moves; Changed the instance variable @squares to the class variable @@squares and the instance method Board#find to the class method Board#self.find to simplify the process of finding a Square object from outside the Board class; Knight#possible_moves now returns an array of Square objects on the Board that the knight is allowed to move to; Created Knight#moves_tree, which will contain nodes of possible paths from the current square to another square (currently incomplete); Added Class Node, whose instances will be stored in the Knight's moves tree -- each node's children are possible squares that the night may move to

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,20 +1,31 @@
 # frozen_string_literal: true
 
+# Method to aid in manipulation of arrays of coordinates (possible moves)
+module Element
+  def type_to_i
+    instance_of?(Array) ? 0 : 1
+  end
+end
+
+class Object
+  include Element
+end
+
 # Creates chess board
 class Board
-  attr_accessor :squares, :knight
+  attr_accessor :knight
 
   def initialize
-    @squares = []
-    8.times { |row| 8.times { |column| @squares << Square.new(row, column) } }
+    @@squares = []
+    8.times { |row| 8.times { |column| @@squares << Square.new(row, column) } }
   end
 
-  def find(row, column)
-    @squares.find { |square| square.coordinates == [row, column] }
+  def self.find(row, column)
+    @@squares.find { |square| square.coordinates == [row, column] }
   end
 
   def add_knight(row, column)
-    @knight = Knight.new(find(row, column))
+    @knight = Knight.new(Board.find(row, column))
   end
 end
 
@@ -29,7 +40,7 @@ end
 
 # Creates the knight (which goes on the chess board)
 class Knight
-  attr_accessor :position
+  attr_accessor :position, :current
 
   def initialize(position)
     @position = position
@@ -40,6 +51,39 @@ class Knight
   def possible_moves
     valid = [[@row + 1, @column + 2], [@row + 1, @column - 2], [@row + 2, @column + 1], [@row + 2, @column - 1],
              [@row - 1, @column + 2], [@row - 1, @column - 2], [@row - 2, @column + 1], [@row - 2, @column - 1]]
-    valid.map { |position| position if position.none? { |coordinate| coordinate.negative? || coordinate > 7 } }.compact
+    valid.map! do |position|
+      position if position.none? { |coordinate| coordinate.negative? || coordinate > 7 }
+    end.sort_by!(&:type_to_i)
+    valid.map! { |position| Board.find(position[0], position[1]) unless position.nil? }
+  end
+
+  def moves_tree
+    valid = possible_moves
+    @current = Node.new(position)
+    @current.one = valid[0]
+    @current.two = valid[1]
+    @current.three = valid[2]
+    @current.four = valid[3]
+    @current.five = valid[4]
+    @current.six = valid[5]
+    @current.seven = valid[6]
+    @current.eight = valid[7]
+  end
+end
+
+# Creates nodes that are contained within the knight's tree of moves
+class Node
+  attr_accessor :one, :two, :three, :four, :five, :six, :seven, :eight
+
+  def initialize(position)
+    @position = position
+    @one = nil
+    @two = nil
+    @three = nil
+    @four = nil
+    @five = nil
+    @six = nil
+    @seven = nil
+    @eight = nil
   end
 end
