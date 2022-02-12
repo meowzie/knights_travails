50a9bd1872a18e354f027d495fc6f64626f40971
meowzie <appenge2@gmail.com>
2022-02-12 22:04:26 +0200

    Removed need for Element module by switching to Enumerable#compact instead of trying to get all nil values at the end of an enumerable; Modified method parameters (aside from Square#new) to accept an array of positions rather than a separate row and column value (to simplify code); Deleted the old Square class and renamed class Node to Square; Knight#moves_tree does not yet provide a data structure containing all possible moves --  however, it can properly find all possible moves and add them to an array that functions similarly to a stack; Removed redundant (so far) properties from class Square (formerly class Node)

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,16 +1,3 @@
-# frozen_string_literal: true
-
-# Method to aid in manipulation of arrays of coordinates (possible moves)
-module Element
-  def type_to_i
-    instance_of?(Array) ? 0 : 1
-  end
-end
-
-class Object
-  include Element
-end
-
 # Creates chess board
 class Board
   attr_accessor :knight
@@ -20,70 +7,55 @@ class Board
     8.times { |row| 8.times { |column| @@squares << Square.new(row, column) } }
   end
 
-  def self.find(row, column)
-    @@squares.find { |square| square.coordinates == [row, column] }
+  def self.find(position)
+    @@squares.find { |square| square.position == position }
   end
 
-  def add_knight(row, column)
-    @knight = Knight.new(Board.find(row, column))
-  end
-end
-
-# Creates squares on the chess board
-class Square
-  attr_accessor :coordinates
-
-  def initialize(horizontal, vertical)
-    @coordinates = [horizontal, vertical]
+  def add_knight(position)
+    @knight = Knight.new(position)
   end
 end
 
 # Creates the knight (which goes on the chess board)
 class Knight
-  attr_accessor :position, :current
+  attr_accessor :row, :column, :position
 
   def initialize(position)
     @position = position
-    @row = position.coordinates[0]
-    @column = position.coordinates[1]
+    @square = Board.find(position)
   end
 
-  def possible_moves
-    valid = [[@row + 1, @column + 2], [@row + 1, @column - 2], [@row + 2, @column + 1], [@row + 2, @column - 1],
-             [@row - 1, @column + 2], [@row - 1, @column - 2], [@row - 2, @column + 1], [@row - 2, @column - 1]]
+  def possible_moves(coordinates)
+    row = coordinates[0]
+    column = coordinates[1]
+    valid = [[row + 1, column + 2], [row + 1, column - 2], [row + 2, column + 1], [row + 2, column - 1],
+             [row - 1, column + 2], [row - 1, column - 2], [row - 2, column + 1], [row - 2, column - 1]]
     valid.map! do |position|
       position if position.none? { |coordinate| coordinate.negative? || coordinate > 7 }
-    end.sort_by!(&:type_to_i)
-    valid.map! { |position| Board.find(position[0], position[1]) unless position.nil? }
+    end.compact!
+    valid.map! { |position| Board.find(position) unless position.nil? }
   end
 
-  def moves_tree
-    valid = possible_moves
-    @current = Node.new(position)
-    @current.one = valid[0]
-    @current.two = valid[1]
-    @current.three = valid[2]
-    @current.four = valid[3]
-    @current.five = valid[4]
-    @current.six = valid[5]
-    @current.seven = valid[6]
-    @current.eight = valid[7]
+  def moves_tree(current, target, visited = [])
+    visited << current
+    return visited.pop if visited.include?(target)
+
+    possible = possible_moves(current)
+    return if possible.all? { |square| visited.include?(square) }
+
+    possible.each do |square|
+      moves_tree(square.position, target, visited) unless visited.include?(square.position)
+    end
   end
 end
 
-# Creates nodes that are contained within the knight's tree of moves
-class Node
-  attr_accessor :one, :two, :three, :four, :five, :six, :seven, :eight
+# Creates squares that are contained within the knight's tree of moves
+class Square
+  attr_accessor :position, :row, :column
 
-  def initialize(position)
-    @position = position
-    @one = nil
-    @two = nil
-    @three = nil
-    @four = nil
-    @five = nil
-    @six = nil
-    @seven = nil
-    @eight = nil
+  def initialize(row, column)
+    @row = row
+    @column = column
+    @position = [@row, @column]
   end
 end
