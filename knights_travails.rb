38cf39707ba17ded2dce3cfe064b9d3790ad05d3
meowzie <appenge2@gmail.com>
2022-03-09 17:44:02 +0200

    Removed Knight class and method to generate possible moves. Will now implement Knight's Travails problem using a predetermined graph (in which possible moves are linked together on the Board) rather than generate moves on the fly as before.

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,7 +1,7 @@
+require 'pry-byebug'
+
 # Creates chess board
 class Board
-  attr_accessor :knight
-
   def initialize
     @@squares = []
     8.times { |row| 8.times { |column| @@squares << Square.new(row, column) } }
@@ -16,46 +16,9 @@ class Board
   end
 end
 
-# Creates the knight (which goes on the chess board)
-class Knight
-  attr_accessor :row, :column, :position
-
-  def initialize(position)
-    @position = position
-    @square = Board.find(position)
-  end
-
-  def possible_moves(coordinates)
-    row = coordinates[0]
-    column = coordinates[1]
-    valid = [[row + 1, column + 2], [row + 1, column - 2], [row + 2, column + 1], [row + 2, column - 1],
-             [row - 1, column + 2], [row - 1, column - 2], [row - 2, column + 1], [row - 2, column - 1]]
-    valid.map! do |position|
-      position if position.none? { |coordinate| coordinate.negative? || coordinate > 7 }
-    end.compact!
-    valid.map! { |position| Board.find(position) unless position.nil? }
-  end
-
-  def moves_tree(current, target, visited = [])
-    visited << current
-    return visited.pop if visited.include?(target)
-
-    possible = possible_moves(current)
-    return if possible.all? { |square| visited.include?(square) }
-
-    possible.each do |square|
-      moves_tree(square.position, target, visited) unless visited.include?(square.position)
-    end
-  end
-end
-
 # Creates squares that are contained within the knight's tree of moves
 class Square
-  attr_accessor :position, :row, :column
 
   def initialize(row, column)
-    @row = row
-    @column = column
-    @position = [@row, @column]
   end
 end
