935dbde7414aef6d75d68c51fe688d4e9c981e94
meowzie <appenge2@gmail.com>
2022-03-09 17:45:51 +0200

    removed unnecessary methods from Board class

--- knights_travails.rb
+++ knights_travails.rb
@@ -3,22 +3,13 @@ require 'pry-byebug'
 # Creates chess board
 class Board
   def initialize
-    @@squares = []
     8.times { |row| 8.times { |column| @@squares << Square.new(row, column) } }
-  end
-
-  def self.find(position)
-    @@squares.find { |square| square.position == position }
-  end
-
-  def add_knight(position)
-    @knight = Knight.new(position)
+    @intial = nil
   end
 end
 
 # Creates squares that are contained within the knight's tree of moves
 class Square
-
   def initialize(row, column)
   end
 end
